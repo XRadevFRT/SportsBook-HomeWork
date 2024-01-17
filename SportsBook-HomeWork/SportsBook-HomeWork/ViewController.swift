aed1d1b31419f3a7969755dbec0898b998e00a5e
Radoslav Radev <radoslav.radev@scalefocus.com>
2024-01-17 16:48:14 +0200

    Base iOS project setup

---
+++ SportsBook-HomeWork/SportsBook-HomeWork/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SportsBook-HomeWork
+//
+//  Created by Radoslav Radev  on 17.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .purple
+    }
+}
