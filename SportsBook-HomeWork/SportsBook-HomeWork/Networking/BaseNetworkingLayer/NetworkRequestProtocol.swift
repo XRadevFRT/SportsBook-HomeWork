05f8cd6ddb4eb96a1964b3b3ff24f8183290e7c7
Radoslav Radev <radoslav.radev@scalefocus.com>
2024-01-18 17:29:15 +0200

    [Networking] Update NetworkRequestProtocol

--- SportsBook-HomeWork/SportsBook-HomeWork/Networking/BaseNetworkingLayer/NetworkRequestProtocol.swift
+++ SportsBook-HomeWork/SportsBook-HomeWork/Networking/BaseNetworkingLayer/NetworkRequestProtocol.swift
@@ -8,9 +8,32 @@
 import Foundation
 
 protocol NetworkRequestProtocol {
-    var baseURL: URL { get }
+    var baseURL: URL? { get }
     var path: String { get }
     var method: HTTPMethod { get }
     var headers: [String: String] { get }
     var parameters: [String: Any]? { get }
+    var requiresAuthorization: Bool { get }
+}
+
+
+extension NetworkRequestProtocol {
+    var baseURL: URL? {
+        URL(string: "http://localhost:8080")
+    }
+
+    var headers: [String: String] {
+        guard requiresAuthorization else {
+            return [:]
+        }
+        return ["Authorization": "Bearer ewogICAibmFtZSI6ICJHdWVzdCIKfQ=="]
+    }
+
+    var parameters: [String: Any]? {
+        nil
+    }
+
+    var requiresAuthorization: Bool {
+        false
+    }
 }
