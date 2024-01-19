ec000608063a062ec43f1939433e8c3ab9f69e29
Radoslav Radev <radoslav.radev@scalefocus.com>
2024-01-19 08:17:19 +0200

    [Modules] Add Activable to be able to easily show Activity indicator

---
+++ SportsBook-HomeWork/SportsBook-HomeWork/Utilities/Activable.swift
@@ -0,0 +1,65 @@
+//
+//  Activable.swift
+//  SportsBook-HomeWork
+//
+//  Created by Radoslav Radev  on 19.01.24.
+//
+
+import UIKit
+
+public let activityViewTag = (Int.max - 1)
+
+public protocol Activable: AnyObject {
+    func startActivityIndicator()
+    func stopActivityIndicator()
+}
+
+extension UIView: Activable {
+    @objc open func startActivityIndicator() {
+        if let existingActivityView = viewWithTag(activityViewTag) {
+            existingActivityView.removeFromSuperview()
+        }
+
+        let activityView = UIView(frame: bounds)
+        var frame: CGRect = activityView.frame
+        frame.origin.y -= 50
+        frame.size.height += 100
+        activityView.frame = frame
+        activityView.backgroundColor = UIColor.clear
+        activityView.alpha = 0
+        activityView.tag = activityViewTag
+        addSubview(activityView)
+        let spinner = UIActivityIndicatorView(style: .medium)
+        activityView.addSubview(spinner)
+        spinner.center = activityView.center
+        spinner.autoresizingMask = .flexibleTopMargin
+        spinner.startAnimating()
+        UIView.animate(withDuration: 0.2, animations: {
+            activityView.alpha = 1
+        })
+    }
+
+    @objc open func stopActivityIndicator() {
+        let activityView = viewWithTag(activityViewTag)
+
+        if let activityView = activityView {
+            UIView.animate(withDuration: 0.2, animations: {
+                activityView.alpha = 0
+            }, completion: { _ in
+                activityView.removeFromSuperview()
+            })
+        }
+    }
+}
+
+extension Activable where Self: UIViewController {
+    public func startActivityIndicator() {
+        view.startActivityIndicator()
+    }
+
+    public func stopActivityIndicator() {
+        DispatchQueue.main.async { [weak self] in
+            self?.view.stopActivityIndicator()
+        }
+    }
+}
