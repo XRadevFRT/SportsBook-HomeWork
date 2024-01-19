90e410735c985a6a8118a869ea5a24965d9d5d9a
Radoslav Radev <radoslav.radev@scalefocus.com>
2024-01-19 08:21:00 +0200

    [Modules] Add and Implement StatusScreenModule

---
+++ SportsBook-HomeWork/SportsBook-HomeWork/Modules/StatusScreen/StatusScreenInteractor.swift
@@ -0,0 +1,42 @@
+//
+//  StatusScreenInteractor.swift
+//  SportsBook-HomeWork
+//
+//  Created by Radoslav Radev  on 19.01.24.
+//
+
+import Foundation
+
+protocol StatusScreenInteractorOutput: AnyObject {
+    func getAPIStatusSuccess(_ result: APIStatus)
+    func getAPIStatusFailed()
+}
+
+protocol StatusScreenInteractorInput: AnyObject {
+    func getAPIStatus()
+}
+
+final class StatusScreenInteractor {
+    private var getStatusService: GetStatusServiceProtocol
+
+    weak var output: StatusScreenInteractorOutput?
+
+    init(getStatusService: GetStatusServiceProtocol) {
+        self.getStatusService = getStatusService
+    }
+}
+
+// MARK: - StatusScreenInteractorInput
+
+extension StatusScreenInteractor: StatusScreenInteractorInput {
+    func getAPIStatus() {
+        getStatusService.getStatus { [weak self] result in
+            switch result {
+            case .success(let result):
+                self?.output?.getAPIStatusSuccess(result)
+            case .failure:
+                self?.output?.getAPIStatusFailed()
+            }
+        }
+    }
+}
