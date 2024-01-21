67faefde5b55eed013a960541c1335d82627e3b2
Radoslav Radev <radoslav.radev@scalefocus.com>
2024-01-21 16:24:25 +0200

    [Refactoring] Rework the GenericTableViewCell

--- SportsBook-HomeWork/SportsBook-HomeWork/Utilities/GenericTableViewCell.swift
+++ SportsBook-HomeWork/SportsBook-HomeWork/Utilities/GenericTableViewCell.swift
@@ -7,33 +7,18 @@
 
 import UIKit
 
-class GenericTableViewCell<View: UIView>: UITableViewCell {
+final class GenericTableViewCell<View: UIView>: UITableViewCell {
 
-    // Create a property to hold the embedded view
-    var embeddedView = View()
+    // MARK: - Private properties
 
-    // The container view will hold the embeddedView and the separators. That way we can easily work with custom paddings/
-    var containerView: UIView = {
+    // The container view will hold the embeddedView and the separators. That way we can easily work with custom paddings
+    private var containerView: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
 
-    // Separator configuration properties
-    var separatorColor: UIColor = .gray {
-        didSet {
-            topSeparatorView.backgroundColor = separatorColor
-            bottomSeparatorView.backgroundColor = separatorColor
-        }
-    }
-
-    var separatorSize: CGFloat = 1.0 {
-        didSet {
-            topSeparatorHeightConstraint?.constant = separatorSize
-            bottomSeparatorHeightConstraint?.constant = separatorSize
-            layoutIfNeeded()
-        }
-    }
+    private var constraintsHolder: [NSLayoutConstraint] = []
 
     // Top and bottom separators
     private var topSeparatorView: UIView = {
@@ -48,69 +33,92 @@ class GenericTableViewCell<View: UIView>: UITableViewCell {
         return view
     }()
 
-    // Padding configuration properties
-    var contentPadding: UIEdgeInsets = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8) {
+    // MARK: - Public properties
+
+    var embeddedView = View()
+
+    var insets: UIEdgeInsets = .zero {
         didSet {
-            updateContainerPadding()
+            if insets != oldValue {
+                setContainerConstraints()
+            }
         }
     }
 
-    private var topSeparatorHeightConstraint: NSLayoutConstraint?
-    private var bottomSeparatorHeightConstraint: NSLayoutConstraint?
-    private var containerViewConstraints: [NSLayoutConstraint] = []
+    var separatorHeight: CGFloat = 1.0 {
+        didSet {
+            setContainerConstraints()
+
+        }
+    }
+
+    var separatorColor: UIColor = Theme.Colors.commonGrey {
+        didSet {
+            topSeparatorView.backgroundColor = separatorColor
+            bottomSeparatorView.backgroundColor = separatorColor
+        }
+    }
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setupUI()
+        selectionStyle = .none
     }
 
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        setupUI()
+    required init?(coder: NSCoder) {
+        fatalError("GenericTableViewCell<ViewL UIView>.init(coder:) has not been implemented")
     }
+
+    // MARK: - Private functions
 
     private func setupUI() {
         contentView.addSubview(containerView)
-        containerViewConstraints = paddingConfiguredContainerViewConstraints
+        containerView.addSubview(embeddedView)
+        embeddedView.translatesAutoresizingMaskIntoConstraints = false
 
         setupTopSeparatorView()
         setupBottomSeparatorView()
-        setupEmbeddedView()
+        setupTapGestureForTapAnimatedView()
 
-        NSLayoutConstraint.activate(
-            containerViewConstraints +
-            topSeparatorConstraints +
-            bottomSeparatorConstraints +
-            embeddedViewConstraints
-        )
+        setContainerConstraints()
+    }
+
+    private func setupTapGestureForTapAnimatedView() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTapGesture))
+        self.addGestureRecognizer(tapGesture)
+        self.isUserInteractionEnabled = true
+    }
+
+    @objc private func handleTapGesture() {
+        guard let tapAnimatedView = embeddedView as? TapAnimatedView else {
+            return
+        }
+        tapAnimatedView.initiateTapAction()
     }
 
     private func setupTopSeparatorView() {
-        contentView.addSubview(topSeparatorView)
+        containerView.addSubview(topSeparatorView)
         topSeparatorView.backgroundColor = separatorColor
-        topSeparatorView.isHidden = true
-        topSeparatorHeightConstraint = topSeparatorView.heightAnchor.constraint(equalToConstant: separatorSize)
+        topSeparatorView.isHidden = false
     }
 
     private func setupBottomSeparatorView() {
-        contentView.addSubview(bottomSeparatorView)
+        containerView.addSubview(bottomSeparatorView)
         bottomSeparatorView.backgroundColor = separatorColor
         bottomSeparatorView.isHidden = false
-        bottomSeparatorHeightConstraint = bottomSeparatorView.heightAnchor.constraint(equalToConstant: separatorSize)
     }
 
-    private func setupEmbeddedView() {
-        containerView.addSubview(embeddedView)
-        embeddedView.translatesAutoresizingMaskIntoConstraints = false
+    private func setContainerConstraints() {
+        NSLayoutConstraint.deactivate(constraintsHolder)
+        constraintsHolder.removeAll()
+        constraintsHolder.append(contentsOf: containerViewConstraints)
+        constraintsHolder.append(contentsOf: embeddedViewConstraints)
+        constraintsHolder.append(contentsOf: topSeparatorConstraints)
+        constraintsHolder.append(contentsOf: bottomSeparatorConstraints)
+        NSLayoutConstraint.activate(constraintsHolder)
     }
 
-    private func updateContainerPadding() {
-        NSLayoutConstraint.deactivate(containerViewConstraints)
-        containerViewConstraints.removeAll()
-
-        containerViewConstraints = paddingConfiguredContainerViewConstraints
-        NSLayoutConstraint.activate(containerViewConstraints)
-    }
+    // MARK: - Public functions
 
     func configureSeparators(isFirst: Bool, isLast: Bool) {
         topSeparatorView.isHidden = isFirst
@@ -118,13 +126,16 @@ class GenericTableViewCell<View: UIView>: UITableViewCell {
     }
 }
 
+// MARK: - Constrain providers
+
 private extension GenericTableViewCell {
+
     var topSeparatorConstraints: [NSLayoutConstraint] {
         [
             topSeparatorView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
             topSeparatorView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
-            topSeparatorView.topAnchor.constraint(equalTo: contentView.topAnchor),
-            topSeparatorHeightConstraint!
+            topSeparatorView.topAnchor.constraint(equalTo: containerView.topAnchor),
+            topSeparatorView.heightAnchor.constraint(equalToConstant: separatorHeight)
         ]
     }
 
@@ -132,26 +143,26 @@ private extension GenericTableViewCell {
         [
             bottomSeparatorView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
             bottomSeparatorView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
-            bottomSeparatorView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
-            bottomSeparatorHeightConstraint!
+            bottomSeparatorView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
+            bottomSeparatorView.heightAnchor.constraint(equalToConstant: separatorHeight)
+        ]
+    }
+
+    var containerViewConstraints: [NSLayoutConstraint] {
+        [
+            containerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: insets.left),
+            containerView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            containerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -insets.right),
+            containerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
         ]
     }
 
     var embeddedViewConstraints: [NSLayoutConstraint] {
         [
-            embeddedView.topAnchor.constraint(equalTo: topSeparatorView.bottomAnchor),
-            embeddedView.bottomAnchor.constraint(equalTo: bottomSeparatorView.topAnchor),
+            embeddedView.topAnchor.constraint(equalTo: containerView.topAnchor, constant: separatorHeight + insets.top),
             embeddedView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
-            embeddedView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor)
-        ]
-    }
-
-    var paddingConfiguredContainerViewConstraints: [NSLayoutConstraint] {
-        [
-            containerView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: contentPadding.top),
-            containerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: contentPadding.left),
-            containerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -contentPadding.right),
-            containerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -contentPadding.bottom)
+            embeddedView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+            embeddedView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -separatorHeight - insets.bottom)
         ]
     }
 }
