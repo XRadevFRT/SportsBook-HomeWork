0578cc1e4bfac93cc177410b571aae44aa5573c3
Radoslav Radev <radoslav.radev@scalefocus.com>
2024-01-21 16:30:40 +0200

    [Modules] Add and implement SportsList module

---
+++ SportsBook-HomeWork/SportsBook-HomeWork/Modules/SportsList/SportsListPresenter.swift
@@ -0,0 +1,65 @@
+//
+//  SportsListPresenter.swift
+//  SportsBook-HomeWork
+//
+//  Created by Radoslav Radev  on 20.01.24.
+//
+
+import Foundation
+
+final class SportsListPresenter {
+    unowned var view: SportsListViewControllerInput
+    let interactor: SportsListInteractorInput
+    let router: SportsListRouterInput
+
+    private let completionHandler: SportsListBuilderCompletionHandler
+
+    init(view: SportsListViewControllerInput, 
+         interactor: SportsListInteractorInput,
+         router: SportsListRouterInput,
+         completionHandler: @escaping SportsListBuilderCompletionHandler) {
+        self.view = view
+        self.interactor = interactor
+        self.router = router
+        self.completionHandler = completionHandler
+    }
+
+    private func callGetSportList() {
+        view.startActivityIndicator()
+        interactor.getSportsList()
+    }
+}
+
+// MARK: - SportsListViewControllerOutput
+
+extension SportsListPresenter: SportsListViewControllerOutput {
+    func didSelectSport(_ sportId: Int) {
+        completionHandler(.sportSelected(sportId))
+    }
+    
+    func viewIsReady() {
+        callGetSportList()
+    }
+}
+
+// MARK: - SportsListInteractorOutput
+
+extension SportsListPresenter: SportsListInteractorOutput {
+    func getSportsSuccess(_ result: [Sport]) {
+        view.stopActivityIndicator()
+        view.updateUI(sports: result)
+    }
+    
+    func getSportsFailed() {
+        view.stopActivityIndicator()
+        router.showFailedRequestAlert()
+    }
+}
+
+// MARK: - SportsListRouterOutput
+
+extension SportsListPresenter: SportsListRouterOutput {
+    func didPressRetry() {
+        callGetSportList()
+    }
+}
