dbc02457d5cc069d2c4e29f991704c825a85c29f
Radoslav Radev <radoslav.radev@scalefocus.com>
2024-01-21 15:54:50 +0200

    [Utilities] Add Theme that will hold the apps theme components

---
+++ SportsBook-HomeWork/SportsBook-HomeWork/App/Base/Theme.swift
@@ -0,0 +1,26 @@
+//
+//  Theme.swift
+//  SportsBook-HomeWork
+//
+//  Created by Radoslav Radev  on 21.01.24.
+//
+
+import UIKit.UIColor
+
+enum Theme {
+
+    enum Colors {
+        static let commonGrey: UIColor = UIColor(red: 230/255.0, green: 230/255.0, blue: 230/255.0, alpha: 1.0)
+    }
+
+    enum Spacings {
+        static let standardCellHSpacing: UIEdgeInsets = .init(top: 20, left: 20, bottom: 20, right: 20)
+    }
+
+    enum CornerRadiusProvider {
+        static let defaultRadius: CGFloat = 8
+
+    }
+}
+
+
