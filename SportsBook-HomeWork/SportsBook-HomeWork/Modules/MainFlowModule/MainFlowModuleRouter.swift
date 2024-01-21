7e27336b326d8027ee5af9e4e11a1c1fb1dba34e
Radoslav Radev <radoslav.radev@scalefocus.com>
2024-01-21 16:31:04 +0200

    [Modules] Update the MainFlow module to use the new SportList module

--- SportsBook-HomeWork/SportsBook-HomeWork/Modules/MainFlowModule/MainFlowModuleRouter.swift
+++ SportsBook-HomeWork/SportsBook-HomeWork/Modules/MainFlowModule/MainFlowModuleRouter.swift
@@ -9,24 +9,31 @@ import UIKit
 
 protocol MainFlowModuleRouterInput: AnyObject {
     func showInitialScreen(from window: UIWindow?)
+    func showSportsListScreen()
 }
 
 protocol MainFlowRouterOutput: AnyObject {
     func statusScreenFinished()
+    func selectedSport(_ sportId: Int)
 }
 
 final class MainFlowModuleRouter {
 
-    private var statusScreenBuilder: StatusScreenBuilderBuildable
+    private var statusScreenBuilder: StatusScreenBuildable
+    private var sportsListBuilder: SportsListBuildable
 
     private let mainNavigationController: UINavigationController
 
     weak var output: MainFlowRouterOutput?
 
-    init(statusScreenBuilder: StatusScreenBuilderBuildable, 
-         mainNavigationController: UINavigationController) {
+    init(statusScreenBuilder: StatusScreenBuildable, 
+         sportsListBuilder: SportsListBuildable, 
+         mainNavigationController: UINavigationController,
+         output: MainFlowRouterOutput? = nil) {
         self.statusScreenBuilder = statusScreenBuilder
+        self.sportsListBuilder = sportsListBuilder
         self.mainNavigationController = mainNavigationController
+        self.output = output
     }
 }
 
@@ -34,13 +41,24 @@ final class MainFlowModuleRouter {
 
 extension MainFlowModuleRouter: MainFlowModuleRouterInput {
     func showInitialScreen(from window: UIWindow?) {
-
-        let statusScreenViewController = statusScreenBuilder.build(handler: { [weak self] in
-            self?.output?.statusScreenFinished()
+        let statusScreenViewController = statusScreenBuilder.build(handler: { [weak output] in
+            output?.statusScreenFinished()
         })
 
         mainNavigationController.viewControllers = [statusScreenViewController]
         window?.rootViewController = mainNavigationController
         window?.makeKeyAndVisible()
+    }
+
+    func showSportsListScreen() {
+        let sportsListViewController = sportsListBuilder.build { [weak output] result in
+            switch result {
+            case .sportSelected(let sportId):
+                output?.selectedSport(sportId)
+            }
+        }
+
+        mainNavigationController.viewControllers = [sportsListViewController]
+
     }
 }
