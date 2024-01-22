70d4408182bf46c6b5342a5da552dc9b1bfef1dc
Radoslav Radev <radoslav.radev@scalefocus.com>
2024-01-22 09:18:28 +0200

    [Modules] Add EventDay model

---
+++ SportsBook-HomeWork/SportsBook-HomeWork/Models/EventDay.swift
@@ -0,0 +1,13 @@
+//
+//  EventDay.swift
+//  SportsBook-HomeWork
+//
+//  Created by Radoslav Radev  on 22.01.24.
+//
+
+import Foundation
+
+struct EventDay {
+    let dateText: String
+    let events: [Event]
+}
