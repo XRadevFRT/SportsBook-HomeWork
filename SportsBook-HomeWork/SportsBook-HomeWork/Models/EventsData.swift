ec369e46a63afdb8029fb3ce75c82c0eeba8bbca
Radoslav Radev <radoslav.radev@scalefocus.com>
2024-01-22 09:18:28 +0200

    [Modules] Add EventDay model

--- SportsBook-HomeWork/SportsBook-HomeWork/Models/EventsData.swift
+++ SportsBook-HomeWork/SportsBook-HomeWork/Models/EventsData.swift
@@ -10,3 +10,24 @@ import Foundation
 struct EventsData: Codable {
     let data: [Event]
 }
+
+extension EventsData {
+    func groupEventsByDay() -> [EventDay] {
+        var eventsByDay: [String: [Event]] = [:]
+
+        for event in data {
+            let dateText = event.date.formattedString()
+            if eventsByDay[dateText] == nil {
+                eventsByDay[dateText] = [event]
+            } else {
+                eventsByDay[dateText]?.append(event)
+            }
+        }
+
+        let eventDays: [EventDay] = eventsByDay.map { (dateText, events) in
+            return EventDay(dateText: dateText, events: events)
+        }
+
+        return eventDays
+    }
+}
